af56ccc42bcf6ab4a04e1b3668d60ce22b4fb1fd
CompusolucionesUnity <omendozacs@gmail.com>
2026-04-09 22:30:30 -0600

    New Test Build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "F2M-KeeSafety_VirtualShowroom-0.1.0";
+const contentToCache = [
+    "Build/Kee_Data_Centers.loader.js",
+    "Build/Kee_Data_Centers.framework.js",
+    "Build/Kee_Data_Centers.data",
+    "Build/Kee_Data_Centers.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
